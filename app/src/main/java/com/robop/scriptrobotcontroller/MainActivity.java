2a978683333dc411135ea8edc50ca3a0dda96056
Kawaguchi410 <kawakawa.410487@gmail.com>
2017-11-30 21:02:39 +0900

    dev Kara pull Shita

--- app/src/main/java/com/robop/scriptrobotcontroller/MainActivity.java
+++ app/src/main/java/com/robop/scriptrobotcontroller/MainActivity.java
@@ -1,7 +1,7 @@
 package com.robop.scriptrobotcontroller;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
