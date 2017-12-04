316a0ae5fbe2a905f8471796723b87d56a22abd1
Kawaguchi410 <kawakawa.410487@gmail.com>
2017-12-04 17:03:00 +0900

    基æ開発する際の元ã元を実装

--- app/src/main/java/com/robop/scriptrobotcontroller/MainActivity.java
+++ app/src/main/java/com/robop/scriptrobotcontroller/MainActivity.java
@@ -2,12 +2,100 @@ package com.robop.scriptrobotcontroller;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.widget.ListView;
+        import android.widget.Toast;
+
+        import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = findViewById(R.id.listView);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button startButton = findViewById(R.id.startButton);
+        Button finishButton = findViewById(R.id.finishButton);
+
+        //listの処理
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(getApplicationContext(), "position = " + i, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        listView.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        int num = i;
+                        Toast.makeText(getApplicationContext(), "text = " + num, Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                }
+        );
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.add("1");
+                listView.setAdapter(adapter);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.add("2");
+                listView.setAdapter(adapter);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.add("3");
+                listView.setAdapter(adapter);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.add("4");
+                listView.setAdapter(adapter);
+            }
+        });
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+
+
+
 }
