b64ea7ad6d9e44a1d12f3257c553232f493f9304
Kawaguchi410 <kawakawa.410487@gmail.com>
2018-01-15 20:39:08 +0900

    メインのオンクリックリスナーをまとめました

--- app/src/main/java/com/robop/scriptrobotcontroller/MainActivity.java
+++ app/src/main/java/com/robop/scriptrobotcontroller/MainActivity.java
@@ -2,6 +2,8 @@ package com.robop.scriptrobotcontroller;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
         import android.view.View;
         import android.widget.AdapterView;
         import android.widget.ArrayAdapter;
@@ -13,22 +15,39 @@ package com.robop.scriptrobotcontroller;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    ItemDataList item;
+    private ListView listView;
+    private ListAdapter listAdapter;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ListView listView = findViewById(R.id.listView);
+        //final ListView listView = findViewById(R.id.listView);
+        listView = findViewById(R.id.listView);
+        //RecyclerView recyclerView = findViewById(R.id.listView);
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1);
 
-        final ItemDataList item = new ItemDataList();
-        final ListAdapter listAdapter = new ListAdapter(getApplicationContext(),item);
+        item = new ItemDataList();
+        listAdapter = new ListAdapter(getApplicationContext(),item);
+        listView.setAdapter(listAdapter);
 
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
+        //recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerAdapter = new RecyclerAdapter(item);
+
+        //Button button1 = findViewById(R.id.button1);
+        setButtonLisetener(10, 100, "1個目", R.id.button1);
+        setButtonLisetener(20, 200, "2個目", R.id.button2);
+        setButtonLisetener(30, 300, "3個目", R.id.button3);
+        setButtonLisetener(40, 400, "4個目", R.id.button4);
+
         Button startButton = findViewById(R.id.startButton);
         Button finishButton = findViewById(R.id.finishButton);
 
@@ -51,42 +70,42 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                item.addSpeed(10);
-                item.addTime(100);
-                item.addImageId("1個目");
-                listView.setAdapter(listAdapter);
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                item.addSpeed(20);
-                item.addTime(200);
-                item.addImageId("2個目");
-                listView.setAdapter(listAdapter);
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                item.addSpeed(30);
-                item.addTime(300);
-                item.addImageId("3個目");
-                listView.setAdapter(listAdapter);
-            }
-        });
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                item.addSpeed(40);
-                item.addTime(400);
-                item.addImageId("4個目");
-                listView.setAdapter(listAdapter);
-            }
-        });
+////        button1.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                item.addSpeed(10);
+////                item.addTime(100);
+////                item.addImageId("1個目");
+////                listView.setAdapter(listAdapter);
+////            }
+////        });
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                item.addSpeed(20);
+//                item.addTime(200);
+//                item.addImageId("2個目");
+//                listView.setAdapter(listAdapter);
+//            }
+//        });
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                item.addSpeed(30);
+//                item.addTime(300);
+//                item.addImageId("3個目");
+//                listView.setAdapter(listAdapter);
+//            }
+//        });
+//        button4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                item.addSpeed(40);
+//                item.addTime(400);
+//                item.addImageId("4個目");
+//                listView.setAdapter(listAdapter);
+//            }
+//        });
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -102,7 +121,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    Button setButtonLisetener(final int speed, final int time, final String imageId, int id){
 
+        Button button = findViewById(id);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                item.addSpeed(speed);
+                item.addTime(time);
+                item.addImageId(imageId);
+                listView.setAdapter(listAdapter);
+            }
+        });
+        return button;
+    }
 
 
 }
