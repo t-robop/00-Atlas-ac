466d6dadb366bd377151a156f37c0ba0b310f64b
multiverse2011 <multiverse2011@gmail.com>
2018-06-23 14:13:29 +0900

    小数の時間表記に対応

--- app/src/main/java/com/robop/scriptrobotcontroller/EditParamDialog.java
+++ app/src/main/java/com/robop/scriptrobotcontroller/EditParamDialog.java
@@ -7,7 +7,9 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.text.InputType;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -77,9 +79,49 @@ public class EditParamDialog extends DialogFragment{
 
 
         final EditText editTime = view.findViewById(R.id.edit_time);
-        editTime.setInputType(InputType.TYPE_CLASS_NUMBER);
+        //小数と整数に対応
+        editTime.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
 
-        editTime.setText(Integer.toString(dataModel.getTime()));
+        InputFilter inputFilter = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                int dotPos = -1;
+                int len = dest.length();
+                for (int i = 0; i < len; i++) {
+                    char c = dest.charAt(i);
+                    if (c == '.' || c == ',') {
+                        dotPos = i;
+                        break;
+                    }
+                }
+                if (dotPos >= 0) {
+
+                    // protects against many dots
+                    if (source.equals(".") || source.equals(","))
+                    {
+                        return "";
+                    }
+                    // if the text is entered before the dot
+                    if (dend <= dotPos) {
+                        return null;
+                    }
+                    if (len - dotPos > 2) {
+                        return "";
+                    }
+                }
+
+                return null;
+            }
+        };
+        // フィルターの配列を作成
+        InputFilter[] filters = new InputFilter[] {
+                inputFilter,
+                new InputFilter.LengthFilter(3)
+        };
+        // フィルターの配列をセット
+        editTime.setFilters(filters);
+
+        editTime.setText(Float.toString(((float) (dataModel.getTime()))/10f));
         editTime.setSelection(editTime.getText().length());
 
         builder.setView(view)
@@ -116,7 +158,7 @@ public class EditParamDialog extends DialogFragment{
                                     break;
 
                             }
-                            dataModel.setTime(Integer.valueOf(editTime.getText().toString()));
+                            dataModel.setTime((int) ((Float.parseFloat(editTime.getText().toString()))*10));
 
                             MainActivity mainActivity = (MainActivity) getActivity();
                             mainActivity.updateItemParam(listItemPosition, dataModel);
